fd902a5ee8c1a29039864428fcdb7ca1644767d6
EunhoMoon <eunho.moon@riskx.tech>
2024-03-21 18:15:24 +0900

    문제 발생

--- src/test/java/com/motivank/stock/service/StockServiceTest.java
+++ src/test/java/com/motivank/stock/service/StockServiceTest.java
@@ -10,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,18 +28,18 @@ class StockServiceTest {
     StockRepository stockRepository;
 
     @BeforeEach
-    public void before() {
+    void before() {
         stockRepository.save(new Stock(1L, 100L));
     }
 
     @AfterEach
-    public void after() {
+    void after() {
         stockRepository.deleteAll();
     }
 
     @Test
     @DisplayName("재고 감소가 정상 동작한다.")
-    public void decrease() {
+    void decrease() {
         // given
         stockService.decrease(1L, 10L);
 
@@ -48,11 +52,36 @@ class StockServiceTest {
 
     @Test
     @DisplayName("재고가 0개 미만이 되면 예외가 발생한다.")
-    public void decreaseException() {
+    void decreaseException() {
         // expect
         assertThatThrownBy(() -> stockService.decrease(1L, 101L))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("재고는 0개 미만이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("동시 요청")
+    void concurrent() throws InterruptedException {
+        // given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(0);
     }
 
 }
