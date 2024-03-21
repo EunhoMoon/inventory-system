7f5c921d3daf28847c91bc19bf972b58fc0061f6
EunhoMoon <eunho.moon@riskx.tech>
2024-03-21 17:45:55 +0900

    init project

---
+++ src/test/java/com/motivank/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivank.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
